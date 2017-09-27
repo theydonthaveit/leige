12f110ac00f9228e2718303cbafaf6b9027f92f5
Alan Williams <theydonthaveit@gmail.com>
2017-09-27 15:06:01 +0100

    placed hapi handlers in separate file for code refactoring

---
+++ API/src/handlers.ts
@@ -0,0 +1,34 @@
+import * as GeoLocal from 'node-where';
+import DBH from './dbh'
+
+export default {
+    generate_base_profile: function(req: any, res: any) {
+        let ipAddressReq: string = req.info.remoteAddress
+        let geoPostCodeRes: string
+        let geoAddressRes: string        
+
+        GeoLocal.is(ipAddressReq, function(err, result) {
+            if (result) {
+                geoPostCodeRes = result.get('postalCode')
+                geoAddressRes = result.get('city')
+            } else {
+                res("You have no location")
+            }
+        })
+
+        let dbhResp = DBH.addUser(ipAddressReq, geoPostCodeRes, geoAddressRes)
+        res(dbhResp)
+    },
+    protect_base_profile: function(req: any, res: any) {
+        let ipAddressReq: string = req.info.remoteAddress
+
+        let dbhResp = DBH.protectYourProfile(req.payload, ipAddressReq)
+        res(dbhResp)
+    },
+    build_base_profile: function(req: any, res: any) {
+        let ipAddressReq: string = req.info.remoteAddress
+
+        let dbhResp = DBH.buildYourProfile(req.payload, ipAddressReq)
+        res(dbhResp)
+    }
+}
