c6ba739035289fc7c259aa7a5f0b643b5790b754
Alan Williams <theydonthaveit@gmail.com>
2017-10-12 17:10:03 +0100

    deftones: changes

--- FRONTEND_USER/App.js
+++ FRONTEND_USER/App.js
@@ -1,16 +1,66 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
-import { FormLabel, FormInput, SearchBar } from 'react-native-elements'
+import { ActivityIndicator, StyleSheet, Text, View, Image } from 'react-native';
+import { Card, Button, FormLabel, FormInput, SearchBar } from 'react-native-elements'
+
+// COULD DO A GLOBAL FETCH OT XTHP whatever it is called
 
 class GameCarosal extends Component {
-  // construct CAROSAL STATE
+  constructor() {
+    super()
+
+    this.state = {
+      isLoading: true,
+      gamePicturesUri: null,
+      gameTitle: null
+    }
+  }
+
+  componentDidMount() {
+    // make a request from server and create an array of images
+    // fetch('https://facebook.github.io/react-native/movies.json')
+    //   .then((response) => response.json())
+    //   .then((responseJson) => {
+    //     let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    //     this.setState({
+    //       isLoading: false,
+    //       dataSource: ds.cloneWithRows(responseJson.movies),
+    //     }, function() {
+    //       // do something with new state
+    //     });
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   })
+    this.setState({
+      isLoading: false,
+      gamePicturesUri: 'https://news-a.akamaihd.net/public/images/misc/GameBox.jpg',
+      gameTitle: 'League of Legends'
+    })
+  }
 
   render() {
-    let pic = {
-      uri: 'https://news-a.akamaihd.net/public/images/misc/GameBox.jpg'
+    if (this.state.isLoading) {
+      return (
+        <View>
+          <ActivityIndicator />
+          <Text>We are Loading...</Text>
+        </View>
+      )
     }
+
     return (
-      <Image source={pic} style={styles.image}/>
+      <Card
+      title={this.state.gameTitle}
+      image={{uri: this.state.gamePicturesUri}}>
+        <Text style={{marginBottom: 10}}>
+          The idea with React Native Elements is more about component structure than actual design.
+        </Text>
+        <Button
+          icon={{name: 'code'}}
+          backgroundColor='#03A9F4'
+          buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+          title='ABOUT THIS GAME?' />
+      </Card>
     )
   }
 }
@@ -62,8 +112,9 @@ export default class App extends Component {
 
   render() {
     return (
-      <View>
+      <View style={{flex: 1, backgroundColor: '#000'}}>
         <SearchBar
+          containerStyle={{marginTop: 20}}
           placeholder='Type Here...' />
         <GameCarosal></GameCarosal>
         <LeagueOfLegendsForm
@@ -77,8 +128,10 @@ export default class App extends Component {
 
 
 const styles = StyleSheet.create({
-  test: {fontWeight: 'bold',
-  fontSize: 120},
+  test: {
+    fontWeight: 'bold',
+    fontSize: 120
+  },
   image: {
     height: 350
   }
